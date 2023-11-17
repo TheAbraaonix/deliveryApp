f5a5e064936519b53bdd6d6780f746ae7d66a149
Holanda03 <carlosabraao@hotmail.com.br>
2023-11-17 15:20:16 -0300

    criando o front-end do cardapio

---
+++ Controllers/CardapioController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryApp.Controllers
+{
+    public class CardapioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AdicionarItem()
+        {
+            return View();
+        }
+
+        public IActionResult EditarItem()
+        {
+            return View();
+        }
+
+        public IActionResult DeletarItem()
+        {
+            return View();
+        }
+    }
+}
