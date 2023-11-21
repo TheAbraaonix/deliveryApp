25a6eae928755c43c26c609f4c5a45c6a86d451a
Holanda03 <carlosabraao@hotmail.com.br>
2023-11-20 23:52:17 -0300

    adicionando as funcionalidades do botão apagar

---
+++ Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using DeliveryApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryApp.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CardapioModel> Cardapio { get; set; }
+    }
+}
---
+++ Models/CardapioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DeliveryApp.Models
+{
+    public class CardapioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public double Preco {  get; set; }
+    }
+}
